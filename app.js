c136efde7f6fa335e9c95bbabd804f0fa08cb9c0
Shahrear Ahamed <its.shahrear@gmail.com>
2022-10-19 22:08:39 +0600

    add routers and set in app.js file

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const path = require("path")
 
 const app = express();
 
+// import routers file
+const jobsRouter = require("./routers/jobs.router")
+const managerRouter = require("./routers/manager.router")
+const userRouter = require("./routers/user.router")
+
 app.use(express.json())
 app.use(cors())
+
+// router are here
+
+app.use("/api/v1/jobs", jobsRouter)
+app.use("/api/v1/manager", managerRouter)
+app.use("/api/v1/user", userRouter)
 
 app.get("/", (req, res) => {
     res.status(200).sendFile(path.join(__dirname, "/template/home.html"))
---
+++ routers/jobs.router.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router();
+
+// routers are here
+router.get("/")
+
+
+module.exports = router;
